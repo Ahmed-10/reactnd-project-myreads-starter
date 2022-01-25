74c73e863f2f2a709b1e260f625693979c8b461b
Ahmed-10 <ahmedalaa.10@hotmail.com>
2022-01-25 04:56:27 +0200

    refactor: splite static shelss data to be imported

---
+++ src/utils/bookshelfs.js
@@ -0,0 +1,14 @@
+export const bookshelfs = [
+    {
+        title: 'Currently Reading',
+        shelf: 'currentlyReading'
+    },
+    {
+        title: 'Want to Read',
+        shelf: 'wantToRead'
+    },
+    {
+        title: 'Read',
+        shelf: 'read'
+    }
+];
