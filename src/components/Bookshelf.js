1c5dce81db7bb0a7a6b83b4b65dc161e24dac8f1
Ahmed-10 <ahmedalaa.10@hotmail.com>
2022-01-25 04:58:04 +0200

    feat: get all books and display into shelves

--- src/components/Bookshelf.js
+++ src/components/Bookshelf.js
@@ -1,11 +1,14 @@
 import React from 'react';
+import '../App.css';
 
 const Bookshelf = ({title, children}) => {
     return (
         <div className="bookshelf">
             <h2 className="bookshelf-title">{title}</h2>
             <div className="bookshelf-books">
-                {children}
+                <ol className="books-grid">
+                    {Object.keys(children).length? children : <code>No books currently on this shelf</code>}
+                </ol>
             </div>
         </div>
     );
