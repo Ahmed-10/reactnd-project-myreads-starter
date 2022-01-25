ca0752d8bcc970ac47d5c38fd112401493225469
Ahmed-10 <ahmedalaa.10@hotmail.com>
2022-01-25 04:47:33 +0200

    refactor: split code into components

---
+++ src/components/Bookshelf.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Bookshelf = ({title, children}) => {
+    return (
+        <div className="bookshelf">
+            <h2 className="bookshelf-title">{title}</h2>
+            <div className="bookshelf-books">
+                {children}
+            </div>
+        </div>
+    );
+}
+ 
+export default Bookshelf;
